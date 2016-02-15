42c93cbc1e723f8bde355cbc167fa770bdbf5199
Diego <diego@criasoft.com.br>
2016-02-15 11:29:12 -0200

    ATUAL

--- PHPPDO/admin/cabecalho.php
+++ PHPPDO/admin/cabecalho.php
@@ -2,17 +2,7 @@
 	<nav id="menu-cima"> </nav>
 </div>
 <div id="cabecalho_meio" class="fundo_azul">
-	<section class="busca">	<h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2><h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
-    <h2> PARTE ADMINISTRATIVA </h2></section>
+	<section class="busca">	<h2> PARTE ADMINISTRATIVA </h2></section>
 </div>
 
 <div id="cabecalho_inferior" class="fundo_principal">
