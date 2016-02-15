3b049dd4b384bdd757af7d99af29ecface0595af
Diego <diego@criasoft.com.br>
2016-02-15 11:11:31 -0200

    Mudando o arquivo

--- PHPPDO/admin/cabecalho.php
+++ PHPPDO/admin/cabecalho.php
@@ -1,8 +1,9 @@
 <div id="cabecalho_superior">
 	<nav id="menu-cima"> </nav>
 </div>
-<div id="cabecalho_meio" class="fundo_azul">	
-	<section class="busca">	<h2> PARTE ADMINISTRATIVA </h2></section>
+<div id="cabecalho_meio" class="fundo_azul">
+	<section class="busca">	<h2> PARTE ADMINISTRATIVA </h2> <h2> PARTE ADMINISTRATIVA </h2>
+    <h2> PARTE ADMINISTRATIVA </h2></section>
 </div>
 
 <div id="cabecalho_inferior" class="fundo_principal">
